3f79a7c716e9b8b1ebb887394abdea0fcac9fe7d
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2025-01-23 18:34:15 +0530

    Add files via upload

---
+++ Problem23.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+# Q. Number of Unique Subjects Taught by Each Teacher
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt FROM Teacher
+GROUP BY teacher_id;
